c7b0ebfb413527e0552266d04b0374b1650086d6
LaraMn007 <angielara@unimayor.edu.co>
2025-11-28 12:11:44 -0500

    Update Estadisticas.java

--- BatallaDePoder/src/batalladepoder/Clases/Estadisticas.java
+++ BatallaDePoder/src/batalladepoder/Clases/Estadisticas.java
@@ -8,5 +8,23 @@
  * @author RT5
  */
 public class Estadisticas {
+    private int cartasJugadas;
+    private int cartasGanadas;   
     
+    public int getCartasJugadas() {
+        return cartasJugadas;
+    }
+
+    public void setCartasJugadas(int cartasJugadas) {
+        this.cartasJugadas = cartasJugadas;
+    }
+
+    public int getCartasGanadas() {
+        return cartasGanadas;
+    }
+
+    public void setCartasGanadas(int cartasGanadas) {
+        this.cartasGanadas = cartasGanadas;
+    }
 }
+
